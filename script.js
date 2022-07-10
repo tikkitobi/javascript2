f7df64d3750d8ac255fbd6cfb29f63472071d01d
user <example@example.com>
2022-07-10 09:24:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var name = prompt('Введите ваше имя:');
+var age = prompt('Введите ваш возраст:');
+console.log(name);
+console.log(age);
+var plus = prompt('6+3=?');
+console.log('Вы написали число ' + plus + '\n правильный ответ 9');
+
+
+var minus = prompt('6-3=?');
+console.log('Вы написали число ' + minus + '\n правильный ответ 3');
+
+
+var redouble = prompt('6*3=?');
+console.log('Вы написали число ' + redouble + '\n правильный ответ 3');
+
+
+var division = prompt('6/3=?');
+console.log('Вы написали число ' + division + '\n правильный ответ 3');
+
+
+var division_with = prompt('5%3=?');
+console.log('Вы написали число ' + division_with + '\n правильный ответ 2');
